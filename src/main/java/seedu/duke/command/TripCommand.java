38db741f7f3605bd6ae4c43df1b3957a3d631dc4
Weng Xian Yang <e1398191@u.nus.edu>
2026-03-19 10:47:28 +0800

    Revert "Implemented AddTripCommand and ListTripCommand"
    This reverts commit 455156b03d476c78a36f2b618b1211b090f7f2b7.

--- src/main/java/seedu/duke/command/TripCommand.java
+++ src/main/java/seedu/duke/command/TripCommand.java
@@ -5,10 +5,10 @@ import seedu.duke.model.TripList;
 public abstract class TripCommand {
     protected TripList tripList;
 
+    public abstract String execute();
+
     public TripCommand(TripList tripList) {
         this.tripList = tripList;
     }
-
-    public abstract String execute();
 
 }

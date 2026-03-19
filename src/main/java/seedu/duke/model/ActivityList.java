6a0d0bc50720ed1abf32512e79895c21eb55fee2
5iu5 <e1384442@u.nus.edu>
2026-03-19 15:10:54 +0800

    Add ActivityList class to represent an itinerary of activities

---
+++ src/main/java/seedu/duke/model/ActivityList.java
@@ -0,0 +1,39 @@
+package seedu.duke.model;
+
+import java.util.ArrayList;
+
+public class ActivityList {
+    private final ArrayList<Activity> activities;
+
+    public ActivityList(){
+        activities = new ArrayList<>();
+    }
+
+    public void add(Activity a) {
+        activities.add(a);
+    }
+    
+    public Activity remove(int index) { return activities.remove(index); }
+
+    public Activity get(int index) {
+        return activities.get(index);
+    }
+
+    public int size() {
+        return activities.size();
+    }
+
+    public boolean isEmpty() {
+        return activities.isEmpty();
+    }
+
+    public ArrayList<Activity> findActivities(String keyword) {
+        ArrayList<Activity> matchingActivities = new ArrayList<>();
+        for (Activity a: activities){
+            if (a.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                matchingActivities.add(a);
+            }
+        }
+        return matchingActivities;
+    }
+}

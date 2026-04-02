9fb3f10f5c68cfbe34ab1607de80564e91aa014a
5iu5 <e1384442@u.nus.edu>
2026-04-02 15:43:30 +0800

    Enhance activity list display to include trip date range

--- src/main/java/seedu/traveltrio/command/activity/ListActivityCommand.java
+++ src/main/java/seedu/traveltrio/command/activity/ListActivityCommand.java
@@ -4,9 +4,14 @@ import seedu.traveltrio.TravelTrioException;
 import seedu.traveltrio.model.activity.ActivityList;
 
 public class ListActivityCommand extends ActivityCommand {
+    private final String tripStartDate;
+    private final String tripEndDate;
 
-    public ListActivityCommand(ActivityList activityList){
+    public ListActivityCommand(ActivityList activityList, String tripStartDate, String tripEndDate){
         super(activityList);
+        this.tripStartDate = tripStartDate;
+        this.tripEndDate = tripEndDate;
+
     }
 
     public String execute(String tripName) throws TravelTrioException {
@@ -16,7 +21,8 @@ public class ListActivityCommand extends ActivityCommand {
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append("Itinerary for ").append(tripName).append(":\n");
+        sb.append("Itinerary for [").append(tripName).append("] (")
+                .append(tripStartDate).append(" -> ").append(tripEndDate).append("):\n");
 
         String header = String.format("%-3s | %-25s | %-15s | %-12s | %-18s",
                 "No", "Activity", "Location", "Date", "Time");
--- src/test/java/seedu/traveltrio/command/activity/ListActivityCommandTest.java
+++ src/test/java/seedu/traveltrio/command/activity/ListActivityCommandTest.java
@@ -15,7 +15,7 @@ class ListActivityCommandTest {
     void execute_emptyActivityList_throwsException() {
         Trip trip = new Trip("Winter trip", "2026-12-01", "2026-12-30");
         ActivityList activityList = new ActivityList(trip);
-        ListActivityCommand command = new ListActivityCommand(activityList);
+        ListActivityCommand command = new ListActivityCommand(activityList, trip.getStartDate(), trip.getEndDate());
 
         TravelTrioException exception = assertThrows(TravelTrioException.class,
                 () -> command.execute("Winter trip")
@@ -28,18 +28,20 @@ class ListActivityCommandTest {
     void execute_nonEmptyActivityList_returnsItineraryList() throws TravelTrioException {
         Trip trip = new Trip("Japan Trip", "2026-06-01", "2026-06-15");
         ActivityList activityList = new ActivityList(trip);
-        Activity activity1 = new Activity("Diving", "Okinawa", "2026-06-10", "09:00", "15:00");
-        Activity activity2 = new Activity("Hiking", "Fuji Mountain", "2026-06-13", "13:00", "14:30");
+        Activity activity1 = new Activity("Diving", "Okinawa",
+                "2026-06-10", "09:00", "15:00");
+        Activity activity2 = new Activity("Hiking", "Fuji Mountain",
+                "2026-06-13", "13:00", "14:30");
 
         activityList.add(activity1);
         activityList.add(activity2);
 
-        ListActivityCommand command = new ListActivityCommand(activityList);
+        ListActivityCommand command = new ListActivityCommand(activityList, trip.getStartDate(), trip.getEndDate());
         String result = command.execute("Japan Trip");
 
         String header = String.format("%-3s | %-25s | %-15s | %-12s | %-18s",
                 "No", "Activity", "Location", "Date", "Time");
-        String expected = "Itinerary for Japan Trip:\n"
+        String expected = "Itinerary for [Japan Trip] (2026-06-01 -> 2026-06-15):\n"
                 + header + "\n"
                 + "-".repeat(header.length()) + "\n"
                 + activity1.formatForTableRow(1) + "\n"
